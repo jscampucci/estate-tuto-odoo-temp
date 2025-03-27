b6d4cf7e6133172406fb64e5652e4422801ff02b
jscampucci <joris.scampucci@proton.me>
2025-03-27 15:08:33 +0100

    [init] init estate module + estate.property model

---
+++ __manifest__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Real Elabore",
+    "author": "Odoo",
+    "version": "0.1",
+    "application": True,
+    "installable": True,
+    "depends": ["base"],
+    "data": ["security/ir.model.access.csv", "views/estate_menu.xml"],
+}
---
+++ models/estate_property.py
@@ -0,0 +1,33 @@
+from odoo import models, fields
+import datetime
+
+class EstateProperty(models.Model):
+    _name = 'estate.property'
+    _description = 'Estate Property'
+
+    name = fields.Char('Title')
+    description = fields.Text('Description')
+    postcode = fields.Char('Postcode')
+    date_availability = fields.Date(string='Available From', copy=False, default=(fields.Date.today() + datetime.timedelta(days=90)))
+    expected_price = fields.Float('Expected Price')
+    selling_price = fields.Float(string='Selling Price', readonly=True, copy=False)
+    bedrooms = fields.Integer(string='Bedrooms', default=2)
+    living_area = fields.Integer('Living Area')
+    facades = fields.Integer('Facades')
+    has_garage = fields.Boolean('Garage')
+    has_garden = fields.Boolean('Garden')
+    garden_area = fields.Integer('Garden Area')
+    garden_orientation = fields.Selection([
+        ('north', 'North'),
+        ('south', 'South'),
+        ('east', 'East'),
+        ('west', 'West')
+    ], 'Garden Orientation')
+    status = fields.Selection([
+        ('new', 'New'),
+        ('offer_received', 'Offer Received'),
+        ('offer_accepted', 'Offer Accepted'),
+        ('sold', 'Sold'),
+        ('canceled', 'Canceled')
+    ], 'Status', default='new', copy=False)
+    active = fields.Boolean('Active', default=True)
